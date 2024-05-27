b0dc3cc6d860c58a180e9a7dacab27ce6c023417
nadanasr432 <nadanm432@gmail.com>
2024-05-28 01:38:42 +0300

    front end validation of journal accounts

--- app/Http/Controllers/Client/JournalEntryController.php
+++ app/Http/Controllers/Client/JournalEntryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Client;
 
 use App\Http\Controllers\Controller;
+use App\Models\accounting_tree;
 use Illuminate\Http\Request;
 
 class JournalEntryController extends Controller
@@ -14,7 +15,8 @@ class JournalEntryController extends Controller
 
     public function create_journal_entries()
     {
-        return view('client.journal.create');
+        $accounts = accounting_tree::all();
+        return view('client.journal.create',compact('accounts'));
     }
 
     public function store(Request $request)
--- resources/views/client/journal/create.blade.php
+++ resources/views/client/journal/create.blade.php
@@ -40,22 +40,21 @@
                         @csrf
                         <div class="row p-0 mb-1">
                             <!---ROW1--->
-                            <div class="row  col-12 p-0 pb-2 pl-1 border-bottom border-secondary">
+                            <div class="row col-12 p-0 pb-2 pl-1 border-bottom border-secondary">
                                 <div class="col-md-6 pr-0">
                                     <label> التاريخ <span class="text-danger">*</span></label>
                                     <input type="date" name="date" class="form-control" required>
                                 </div>
 
-                                <div class="col-md-6 pr-0">
+                                {{-- <div class="col-md-6 pr-0">
                                     <label> الزبون/ المورد <span class="text-danger">*</span></label>
                                     <input type="text" placeholder="الزبون/ المورد" class="form-control"
-                                           name="payer_name" style="height: 43px" required>
-                                </div>
+                                        name="payer_name" style="height: 43px" required>
+                                </div> --}}
 
                                 <div class="col-md-12 mt-1 pr-0">
                                     <label> الملاحظات <span class="text-danger">*</span></label>
-                                    <textarea required class="form-control" placeholder="الملاحظات"
-                                              name="description"></textarea>
+                                    <textarea required class="form-control" placeholder="الملاحظات" name="description"></textarea>
                                 </div>
                             </div>
 
@@ -63,63 +62,98 @@
                             <div class="table-responsive pr-1 pl-1">
                                 <table class="table mt-2">
                                     <thead class="table-primary">
-                                    <tr>
-                                        <th scope="col">الحساب</th>
-                                        <th scope="col">مدين</th>
-                                        <th scope="col">دائن</th>
-                                        <th scope="col">ملاحظات</th>
-                                        <th scope="col">تحكم</th>
-                                    </tr>
+                                        <tr>
+                                            <th scope="col">الحساب</th>
+                                            <th scope="col">مدين</th>
+                                            <th scope="col">دائن</th>
+                                            <th scope="col">ملاحظات</th>
+                                            <th scope="col">تحكم</th>
+                                        </tr>
                                     </thead>
                                     <tbody class="tbodyJournals">
-                                    <tr>
-                                        <td>
-                                            <select required name="account[]" class="form-control">
-                                                @include('client.journal.accounts')
-                                            </select>
-                                        </td>
-                                        <td>
-                                            <input type="number" class="form-control" name="debit[]" value="0">
-                                        </td>
-                                        <td>
-                                            <input type="number" class="form-control" name="credit[]" value="0">
-                                        </td>
-                                        <td>
-                                            <input type="text" class="form-control" name="notes[]">
-                                        </td>
-                                        <td>
-                                            <input
-                                                type="button" class="mt-1 btn btn-danger btn-sm deleteRow"
-                                                value="X"/>
-                                        </td>
-                                    </tr>
+                                        <tr>
+                                            <td>
+                                                <select required name="account[]" class="form-control">
+                                                    <option value="">اختر الحساب</option>
+                                                    @foreach ($accounts as $account)
+                                                        <option value="{{ $account->id }}">{{ $account->account_name }}
+                                                        </option>
+                                                    @endforeach
+                                                </select>
+                                            </td>
+                                            <td>
+                                                <input type="number" class="form-control debit" name="debit[]"
+                                                    value="0">
+                                            </td>
+                                            <td>
+                                                <input type="number" class="form-control credit" name="credit[]"
+                                                    value="0">
+                                            </td>
+                                            <td>
+                                                <input type="text" class="form-control" name="notes[]">
+                                            </td>
+                                            <td>
+                                                <input type="button" class="mt-1 btn btn-danger btn-sm deleteRow"
+                                                    value="X" />
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                {{-- <select required name="account[]" class="form-control">
+                                                    @include('client.journal.accounts')
+                                                </select> --}}
+                                                <select required name="account[]" class="form-control">
+                                                    <option value="">اختر الحساب</option>
+                                                    @foreach ($accounts as $account)
+                                                        <option value="{{ $account->id }}">{{ $account->account_name }}
+                                                        </option>
+                                                    @endforeach
+                                                </select>
+                                            </td>
+
+                                            <td>
+                                                <input type="number" class="form-control debit" name="debit[]"
+                                                    value="0">
+                                            </td>
+                                            <td>
+                                                <input type="number" class="form-control credit" name="credit[]"
+                                                    value="0">
+                                            </td>
+                                            <td>
+                                                <input type="text" class="form-control" name="notes[]">
+                                            </td>
+                                            <td>
+                                                <input type="button" class="mt-1 btn btn-danger btn-sm deleteRow"
+                                                    value="X" />
+                                            </td>
+                                        </tr>
                                     </tbody>
                                     <tfoot>
-                                    <tr>
-                                        <td></td>
-                                        <td style="text-align: center">
-                                            المجموع:
-                                            <span id="totalDebit">0</span>
-                                        </td>
-                                        <td style="text-align: center">
-                                            المجموع:
-                                            <span id="totalCredit">0</span>
-                                        </td>
-                                        <td></td>
-                                        <td></td>
-                                    </tr>
+                                        <tr>
+                                            <td></td>
+                                            <td style="text-align: center">
+                                                المجموع:
+                                                <span id="totalDebit">0</span>
+                                            </td>
+                                            <td style="text-align: center">
+                                                المجموع:
+                                                <span id="totalCredit">0</span>
+                                            </td>
+                                            <td id="errorCell" style="color: red;"></td>
+                                            <td></td>
+                                        </tr>
                                     </tfoot>
                                 </table>
 
-                                <button
-                                    type="button" class="btn btn-primary addNewRowHandler">
+                                <button type="button" class="btn btn-primary addNewRowHandler">
                                     <i class="fa fa-plus"></i>
                                     اضافة صف جديد
                                 </button>
                             </div>
                         </div>
                         <div class="col-12 text-center border-top border-secondary mt-1 pt-2 pr-0 pl-0">
-                            <button class="btn btn-success w-100 font-weight-bold" type="submit">{{ __('main.add') }}</button>
+                            <button class="btn btn-success w-100 font-weight-bold"
+                                type="submit">{{ __('main.add') }}</button>
                         </div>
                     </form>
 
@@ -130,19 +164,120 @@
 @endsection
 <script src="{{ asset('app-assets/js/jquery.min.js') }}"></script>
 <script>
-    $(document).ready(function () {
-        $(".addNewRowHandler").click(function () {
-            let accountOptions = '<option value="">اختر الحساب</option> <option value="0">النقدية في الخزينة</option> <option value="1"> العهد النقدية</option> <option value="2"> حساب البنك الجاري - اسم البنك</option> <option value="3"> المدينون</option> <option value="4"> تأمين طبي مقدم</option> <option value="5"> إيجار مقدم</option> <option value="6"> مدفوعات مقدمة للموظفين</option> <option value="7"> المخزون</option> <option value="8"> الأراضي</option> <option value="9"> المباني</option> <option value="10"> المعدات</option> <option value="11"> أجهزة مكتبية وطابعات</option> <option value="12"> الأصول غير الملموسة</option> <option value="13"> العقارات الاستثمارية</option> <option value="14"> الدائنون</option> <option value="15"> مصروفات مستحقة</option> <option value="16"> الرواتب المستحقة</option> <option value="17"> قروض قصيرة الأجل</option> <option value="18"> ضريبة القيمة المضافة المستحقة</option> <option value="19"> الضرائب المستحقة</option> <option value="20"> إيرادات غير مكتسبة</option> <option value="21"> مستحقات المؤسسة العامة للتأمينات الاجتماعية</option> <option value="22"> مجمع استهلاك المباني</option> <option value="23"> مجمع استهلاك المعدات</option> <option value="24"> مجمع استهلاك أجهزة مكتبية وطابعات</option> <option value="25"> مخصص مكافأة نهاية الخدمة</option> <option value="26"> قروض طويلة أجل</option> <option value="27"> رأس المال المسجل</option> <option value="28"> رأس المال الإضافي المدفوع</option> <option value="29"> أرصدة افتتاحية</option> <option value="30"> احتياطي نظامي</option> <option value="31"> احتياطي ترجمة عملات أجنبية</option> <option value="32"> الأرباح والخسائر العاملة</option> <option value="33"> الأرباح المبقاة (أو الخسائر)</option> <option value="34"> إيرادات المبيعات/ الخدمات</option> <option value="35"> تكلفة البضاعة المباعة</option> <option value="36"> رواتب وأجور</option> <option value="37"> عمولات البيع</option> <option value="38"> الرواتب والرسوم الإدارية</option> <option value="39"> إيرادات أخرى</option> <option value="40"> شحن وتخليص جمركي</option> <option value="41"> تأمين طبي</option> <option value="42"> مصاريف تسويقية ودعائية</option> <option value="43"> مصاريف الإيجار</option> <option value="44"> عمولات وحوافز</option> <option value="45"> تذاكر سفر</option> <option value="46"> التأمينات الاجتماعية</option> <option value="47"> الرسوم الحكومية</option> <option value="48"> رسوم واشتراكات</option> <option value="49"> مصاريف مكتبية ومطبوعات</option> <option value="50"> مصاريف خدمات المكتب</option> <option value="51"> مصاريف ضيافة</option> <option value="52"> عمولات بنكية</option> <option value="53"> مصاريف أخرى</option> <option value="54"> مصروف إهلاك المباني</option> <option value="55"> مصروف إهلاك المعدات</option> <option value="56"> مصروف إهلاك أجهزة مكتبية وطابعات</option> <option value="57"> مصروف نقل ومواصلات</option> <option value="58"> الضرائب</option> <option value="59"> الزكاة</option> <option value="60"> ترجمة عملات أجنبية</option> <option value="61"> فوائد</option>';
-            let newRow = '<tr> <td> <select required name="account[]" class="form-control"> ' + accountOptions + ' </select> </td> <td> <input type="number" class="form-control" name="debit[]" value="0"> </td> <td> <input type="number" class="form-control" name="credit[]" value="0"> </td> <td> <input type="text" class="form-control" name="notes[]"> </td> <td> <input type="button" class="mt-1 btn btn-danger btn-sm deleteRow" value="X"/> </td> </tr>';
-            $(".tbodyJournals").append(newRow);
-        });
-        $(".deleteRow").click(function () {
-            $(this).parent().parent().remove();
+    function attachRowEvents() {
+        $('.debit').off('input').on('input', function() {
+            const row = $(this).closest('tr');
+            const creditField = row.find('.credit');
+
+            if ($(this).val() !== '') {
+                creditField.prop('disabled', true).val(0);
+            } else {
+                creditField.prop('disabled', false);
+            }
+            calculateTotal();
         });
 
-        $("#storeJournal").submit(function (e) {
-            e.preventDefault()
-            //start ajax
+        $('.credit').off('input').on('input', function() {
+            const row = $(this).closest('tr');
+            const debitField = row.find('.debit');
+
+            if ($(this).val() !== '') {
+                debitField.prop('disabled', true).val(0);
+            } else {
+                debitField.prop('disabled', false);
+            }
+            calculateTotal();
+        });
+
+        $('.deleteRow').off('click').on('click', function() {
+            $(this).closest('tr').remove();
+        });
+    }
+
+
+    function calculateTotal() {
+        let totalDebit = 0;
+        let totalCredit = 0;
+
+        $('.tbodyJournals tr').each(function() {
+            const debitValue = parseFloat($(this).find('.debit').val()) || 0;
+            const creditValue = parseFloat($(this).find('.credit').val()) || 0;
+
+            totalDebit += debitValue;
+            totalCredit += creditValue;
+        });
+
+        $('#totalDebit').text(totalDebit.toFixed(2));
+        $('#totalCredit').text(totalCredit.toFixed(2));
+
+        if (totalDebit !== totalCredit) {
+            $('#errorCell').text('Error: Total debit is not equal to total credit.');
+        } else {
+            $('#errorCell').text('');
+        }
+    }
+
+
+    $(document).ready(function() {
+       
+        let selectedAccountIds = [];
+
+        function updateSelectedAccountIds() {
+            selectedAccountIds = [];
+            $('select[name="account[]"]').each(function() {
+                let accountId = $(this).val();
+                if (accountId !== '') {
+                    selectedAccountIds.push(accountId);
+                }
+            });
+        }
+
+        function generateAccountOptions() {
+            let accountOptions = '<option value="">اختر الحساب</option>';
+            @foreach ($accounts as $account)
+                if (!selectedAccountIds.includes('{{ $account->id }}')) {
+                    accountOptions +=
+                        '<option value="{{ $account->id }}">{{ $account->account_name }}</option>';
+                }
+            @endforeach
+            return accountOptions;
+        }
+
+        $('select[name="account[]"]').change(function() {
+            updateSelectedAccountIds();
+            let selectedAccountId = $(this).val();
+            $(this).closest('tr').nextAll().find('select[name="account[]"] option').remove();
+            $(this).closest('tr').nextAll().find('select[name="account[]"]').append(
+                generateAccountOptions());
+            $(this).closest('tr').nextAll().find('select[name="account[]"] option[value="' +
+                selectedAccountId + '"]').remove();
+        });
+         attachRowEvents();
+
+        $(".addNewRowHandler").click(function() {
+            updateSelectedAccountIds();
+            let accountOptions = generateAccountOptions();
+
+            let newRow = '<tr> <td> <select required name="account[]" class="form-control"> ' +
+                accountOptions +
+                ' </select> </td> <td> <input type="number" class="form-control debit" name="debit[]" value="0"> </td> <td> <input type="number" class="form-control credit" name="credit[]" value="0"> </td> <td> <input type="text" class="form-control" name="notes[]"> </td> <td> <input type="button" class="mt-1 btn btn-danger btn-sm deleteRow" value="X"/> </td> </tr>';
+            $(".tbodyJournals").append(newRow);
+
+            const lastRow = $(".tbodyJournals tr").eq(-2);
+            const newDebitField = $(".tbodyJournals tr").last().find('.debit');
+            const newCreditField = $(".tbodyJournals tr").last().find('.credit');
+
+            if (lastRow.find('.debit').prop('disabled')) {
+                newCreditField.prop('disabled', false);
+            }
+            if (lastRow.find('.credit').prop('disabled')) {
+                newDebitField.prop('disabled', false);
+            }
+            attachRowEvents();
+        });
+
+        $("#storeJournal").submit(function(e) {
+            e.preventDefault();
             $.ajax({
                 type: "POST",
                 url: "{{ route('client.journal.store') }}",
@@ -150,10 +285,12 @@
                 processData: false,
                 contentType: false,
                 success: (data) => {
+                    alert('تمت العملية بنجاح');
+                },
+                error: (data) => {
+                    alert('حدث خطأ ما');
                 }
-
             });
         });
     });
-
 </script>
